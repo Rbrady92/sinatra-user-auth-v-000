924d89f73ee02dc51f8318c6548208164775fb54
Ryan Brady <ryanbrady369@gmail.com>
2019-05-16 20:29:13 +0000

    Automatically backed up by Learn

--- db/migrate/20190516202233_create_users.rb
+++ db/migrate/20190516202233_create_users.rb
@@ -1,4 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+    create table: Users |t| do 
+      t.string = name 
+      t.string = email 
+      t.string = password 
+    end 
   end
 end
