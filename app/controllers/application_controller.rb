77ae3c078e6f688912d0e10cc98ff0d6af9cfd20
Ryan Brady <ryanbrady369@gmail.com>
2019-05-16 20:58:34 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
   post '/registrations' do
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
-    
+
     session[:user_id] = @user.id
 
     puts params
@@ -49,8 +49,8 @@ class ApplicationController < Sinatra::Base
 
   get '/users/home' do
     @user = User.find(session[:user_id])
-    
+
     erb :'/users/home'
   end
-  
+
 end
